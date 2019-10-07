ce0b94dbb01cd14794caa95756e033f6d9891ef0
isidoreduc <isidoreducasse79@gmail.com>
2019-10-07 22:34:56 +0200

    user data validation

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -4,35 +4,33 @@ using BoardGameDating.api.Models;
 using BoardGameDating.api.Models.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BoardGameDating.api.Controllers
-{
-    [Route("api/[controller]")]
-    public class AuthController : Controller
-    {
+namespace BoardGameDating.api.Controllers {
+    [Route ("api/[controller]")]
+    public class AuthController : Controller {
         private readonly IAuthRepository _authRepo;
 
-        public AuthController(IAuthRepository authRepo)
-        {
+        public AuthController (IAuthRepository authRepo) {
             _authRepo = authRepo;
         }
 
         // FromBody - use the info from the request body (submitted by user)
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody]UserForRegisterDTO userForRegisterDTO)
-        {
-            // data validation
-
-            userForRegisterDTO.Name = userForRegisterDTO.Name.ToLower();
-            if(await _authRepo.UserExists(userForRegisterDTO.Name)) 
-                return BadRequest("Username is taken.");
+        [HttpPost ("register")]
+        public async Task<IActionResult> Register ([FromBody] UserForRegisterDTO userForRegisterDTO) {
+            userForRegisterDTO.Name = userForRegisterDTO.Name.ToLower ();
+            // passing the 'existing user' error in the ModelState towards a BadRequest
+            if (await _authRepo.UserExists (userForRegisterDTO.Name))
+                ModelState.AddModelError("Username", "Username is already taken.");
             
-            var userToCreate = new User()
-            {
+            // data validation = passing the validation error messages in the BadRequest
+            if (!ModelState.IsValid)
+                return BadRequest (ModelState);
+
+            var userToCreate = new User () {
                 Name = userForRegisterDTO.Name
             };
-            var createUser = await _authRepo.Register(userToCreate, userForRegisterDTO.Password);
+            var createUser = await _authRepo.Register (userToCreate, userForRegisterDTO.Password);
 
-            return StatusCode(201);
+            return StatusCode (201);
         }
     }
 }
--- Models/DTOs/UserForRegisterDTO.cs
+++ Models/DTOs/UserForRegisterDTO.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BoardGameDating.api.Models.DTOs
 {
     public class UserForRegisterDTO
     {
+        [Required]
+        [MaxLength(25), MinLength(4)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Please enter a password between 4 and 8 characters")]
         public string Password { get; set; }
     }
 }
